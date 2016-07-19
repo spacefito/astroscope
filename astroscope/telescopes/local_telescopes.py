e47ca833612efd3999717d9a9a8c95b2c7d2c0db
spacefito <adduarte@sbcglobal.net>
2016-07-19 12:28:45 -0700

    Isolating astropy functionality to its own class

--- astroscope/telescopes/local_telescopes.py
+++ astroscope/telescopes/local_telescopes.py
@@ -1,6 +1,7 @@
 from nextstar_telescopes import NexStarSLT130
+from astropy_telescopes import AstropyTelescope
 from astroscope.computers.local import LocalComputer
 
 
-class NexStarSLT130(LocalComputer, NexStarSLT130):
+class NexStarSLT130(LocalComputer, AstropyTelescope, NexStarSLT130):
     pass
