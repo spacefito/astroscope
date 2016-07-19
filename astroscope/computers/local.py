9da8b501727341bd0dfd678088dd07031488aca0
spacefito <adduarte@sbcglobal.net>
2016-07-19 12:30:44 -0700

    isolating functionality

--- astroscope/computers/local.py
+++ astroscope/computers/local.py
@@ -6,16 +6,6 @@ from astropy.coordinates import EarthLocation
 
 class LocalComputer(object):
 
-    def get_time(self):
-        """Get astropy Time object based on telescope settings
-        
-        Queries telescope for time and location. Returns astropy Time object
-        
-        :return astropy object based on telescopes time and location
-        """
-        return Time(self.get_time_initializer(),
-                    location=self.get_earth_location())
-
     def find_view_in_catalog(self, output_filename):
         """Fetches picture from online catalogs of current location telescope is pointing to
         
@@ -32,34 +22,6 @@ class LocalComputer(object):
         with open(output_filename, 'wb') as f:
             for chunck in resp:
                 f.write(chunck)
-
-    def get_earth_location(self):
-        """Returns astropy EarthLocation object of telescope
-
-        :return astropy EarthLocation object
-        """
-        latitude, longitude = self.get_location_lat_long()
-        return EarthLocation(lat=latitude * u.deg, lon=longitude * u.deg)
-
-    def get_azalt(self):
-        """Returns Horizontal SkyCoord telescope is pointing to
-
-         :return Astropy Horizontal SkyCoord
-         """
-        _az, _alt = self.get_az_alt()
-        _altaz = SkyCoord(alt=_alt * u.deg,
-                          az=_az * u.deg,
-                          frame='altaz',
-                          obstime=Time.now(),
-                          location=self.get_earth_location())
-        return _altaz
-
-    def goto_azalt(self, altaz):
-        """Points telescope to given Horizontal SkyCoord coordinates
-        
-        :param altaz: astropy SkyCoordinate object representing the coordinates
-        """
-        self.goto_az_alt(altaz.az.degree, altaz.alt.degree)
 
     def SkyCoordRaDec(self, ra, dec):
         return SkyCoord(ra=_ra * u.deg, dec=_dec * u.deg, frame="icrs")
