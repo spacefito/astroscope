004b100392ad8ffed7ab7b9f6624d626515b88db
spacefito <adduarte@sbcglobal.net>
2016-07-19 12:24:46 -0700

    updated comments

--- astroscope/computers/local.py
+++ astroscope/computers/local.py
@@ -7,10 +7,20 @@ from astropy.coordinates import EarthLocation
 class LocalComputer(object):
 
     def get_time(self):
+        """Get astropy Time object based on telescope settings
+        
+        Queries telescope for time and location. Returns astropy Time object
+        
+        :return astropy object based on telescopes time and location
+        """
         return Time(self.get_time_initializer(),
                     location=self.get_earth_location())
 
     def find_view_in_catalog(self, output_filename):
+        """Fetches picture from online catalogs of current location telescope is pointing to
+        
+        :param output_filename: the filename to save picture to
+        """
         _radec = self.get_radec()
         impix = 1024
         imsize = 12 * u.arcmin
@@ -24,18 +34,17 @@ class LocalComputer(object):
                 f.write(chunck)
 
     def get_earth_location(self):
-        """Returns astropy EarthLocation object
+        """Returns astropy EarthLocation object of telescope
 
-        :return astropy EarthLocation object containg
-        telescopes earth location"""
+        :return astropy EarthLocation object
+        """
         latitude, longitude = self.get_location_lat_long()
         return EarthLocation(lat=latitude * u.deg, lon=longitude * u.deg)
 
     def get_azalt(self):
-        """Returns Horizontal SkyCoord
+        """Returns Horizontal SkyCoord telescope is pointing to
 
-         :return Astropy Horizontal SkyCoord of telescope
-
+         :return Astropy Horizontal SkyCoord
          """
         _az, _alt = self.get_az_alt()
         _altaz = SkyCoord(alt=_alt * u.deg,
@@ -46,6 +55,10 @@ class LocalComputer(object):
         return _altaz
 
     def goto_azalt(self, altaz):
+        """Points telescope to given Horizontal SkyCoord coordinates
+        
+        :param altaz: astropy SkyCoordinate object representing the coordinates
+        """
         self.goto_az_alt(altaz.az.degree, altaz.alt.degree)
 
     def SkyCoordRaDec(self, ra, dec):
