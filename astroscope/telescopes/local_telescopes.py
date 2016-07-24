36552dca3a5c768e436dfb963b8f289d4877f956
spacefito <adduarte@sbcglobal.net>
2016-07-24 15:25:43 -0700

    wip

--- astroscope/telescopes/local_telescopes.py
+++ astroscope/telescopes/local_telescopes.py
@@ -1,6 +1,6 @@
 from nextstar_telescopes import NexStarSLT130
 from astropy_telescope import AstropyTelescope
-from astroscope.computers.local import LocalComputer
+from computers.local import LocalComputer
 
 
 class AstropyNexStarSLT130(LocalComputer, AstropyTelescope, NexStarSLT130):
